d25c2e74d456a23c399f95001a52dfc93797fb35
Goecha <goecha.ksh@gmail.com>
2024-04-11 20:20:53 +0530

    added jasmine testing framework

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -25,7 +25,8 @@ export function renderOrderSummary() {
     const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML +=
-      `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      `<div class="cart-item-container
+      js-cart-item-container js-cart-item-container-${matchingProduct.id}">
           <div class="delivery-date">
             Delivery date: ${dateString}
           </div>
@@ -40,7 +41,7 @@ export function renderOrderSummary() {
               <div class="product-price">
                 $${formatCurrency(matchingProduct.priceCents)}
               </div>
-              <div class="product-quantity">
+              <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                 <span>
                   Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                 </span>
@@ -49,7 +50,7 @@ export function renderOrderSummary() {
                 </span>
                 <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                 <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                   Delete
                 </span>
               </div>
