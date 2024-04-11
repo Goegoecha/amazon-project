0ad2541a2877321588a6d5838303025034dab8be
Goecha <goecha.ksh@gmail.com>
2024-04-11 23:39:25 +0530

    final code cleanup

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -10,14 +10,14 @@ export function renderCheckoutHeader() {
   const checkoutHeaderHTML = `
 <div class="header-content">
   <div class="checkout-header-left-section">
-    <a href="amazon.html">
+    <a href="index.html">
       <img class="amazon-logo" src="images/amazon-logo.png">
         <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
         </a>
       </div>
 
       <div class="checkout-header-middle-section">
-        Checkout (<a class="return-to-home-link js-return-to-home-link" href="amazon.html">${cartQuantity} items</a>)
+        Checkout (<a class="return-to-home-link js-return-to-home-link" href="index.html">${cartQuantity} items</a>)
       </div>
 
       <div class="checkout-header-right-section">
